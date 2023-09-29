b459dfc9c8515580bb52835f35a8c3a2f009bae0
marcelledssantos <marcelledossantos2@gmail.com>
2023-09-29 07:04:39 -0300

    Atualização das paginas home, destinos, promocao e JS

--- Script.js
+++ Script.js
@@ -1,15 +1,73 @@
-const imgs = document.getElementById("img");
-const img= document.querySelectorAll("#img img");
-
-let idx = 0;
-
-function carrossel(){
-    idx++;
-
-    if(idx > img.length - 1) {
-        idx = 0;
-}
-    imgs.style.transform = 'translateX(${-idx * 500}px)';
-}
-
-setInterval(carrossel,1800);
+;(function ( $, window, document, undefined ) {
+ 
+    "use strict";
+ 
+    var
+    pluginName = "banner",
+    defaults = {
+        speed: 2000,
+        delay: 2000
+    },
+    _;
+ 
+    function Plugin ( element, options ) {
+        _ = this;
+        _.banner = $( element );
+        _.wrapper = _.banner.find("#wrapper");
+        _.slides = _.banner.find(".slide");
+        _.count = _.slides.size();
+        _.w = _.banner.width();
+        _.h = _.banner.height();
+        _.settings = $.extend( {}, defaults, options );
+        _.init();
+    }
+ 
+    $.extend(Plugin.prototype, {
+        init: function () {
+            _.setDimensions();
+            _.play();
+        },
+        setDimensions: function () {
+            _.wrapper.css("width", _.count * _.w);
+            _.slides.css("width", _.w);
+        },
+        play: function () {
+            window.setTimeout( function () {
+                _.transition();
+                _.play();
+            }, _.settings.delay + _.settings.speed);
+        },
+        transition: function () {
+ 
+            var value = "-=" + _.w + "px";
+ 
+            if ( _.wrapper.position().left <= - ( _.w * ( _.count - 1 ))) {
+                value = 0;
+            }
+ 
+            _.wrapper.animate({
+                left: value
+            }, _.speed);
+        }
+    });
+ 
+    $.fn[ pluginName ] = function ( options ) {
+        return this.each(function() {
+            if ( !$.data( this, "plugin_" + pluginName ) ) {
+                $.data( this, "plugin_" + pluginName, new Plugin( this, options ) );
+            }
+        });
+    };
+ 
+})( jQuery, window, document );
+ 
+// ------------------------------
+ 
+(function ( $ ) {
+    $(function () {
+        $("#banner").banner({
+            speed: 1500,
+            delay: 1500
+        });
+    });
+})( jQuery );
