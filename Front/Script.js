8db475e6221ce6fe94f4b373ad6ba0d20b4e878a
marcelledssantos <marcelledossantos2@gmail.com>
2023-10-15 21:42:36 -0300

    Ajustes finais das paginas contato,destinos e home

--- Front/Script.js
+++ Front/Script.js
@@ -1,27 +1,27 @@
-;(function ( $, window, document, undefined ) {
- 
+; (function ($, window, document, undefined) {
+
     "use strict";
- 
+
     var
-    pluginName = "banner",
-    defaults = {
-        speed: 2000,
-        delay: 2000
-    },
-    _;
- 
-    function Plugin ( element, options ) {
+        pluginName = "banner",
+        defaults = {
+            speed: 2000,
+            delay: 2000
+        },
+        _;
+
+    function Plugin(element, options) {
         _ = this;
-        _.banner = $( element );
+        _.banner = $(element);
         _.wrapper = _.banner.find("#wrapper");
         _.slides = _.banner.find(".slide");
         _.count = _.slides.size();
         _.w = _.banner.width();
         _.h = _.banner.height();
-        _.settings = $.extend( {}, defaults, options );
+        _.settings = $.extend({}, defaults, options);
         _.init();
     }
- 
+
     $.extend(Plugin.prototype, {
         init: function () {
             _.setDimensions();
@@ -32,42 +32,42 @@
             _.slides.css("width", _.w);
         },
         play: function () {
-            window.setTimeout( function () {
+            window.setTimeout(function () {
                 _.transition();
                 _.play();
             }, _.settings.delay + _.settings.speed);
         },
         transition: function () {
- 
+
             var value = "-=" + _.w + "px";
- 
-            if ( _.wrapper.position().left <= - ( _.w * ( _.count - 1 ))) {
+
+            if (_.wrapper.position().left <= - (_.w * (_.count - 1))) {
                 value = 0;
             }
- 
+
             _.wrapper.animate({
                 left: value
             }, _.speed);
         }
     });
- 
-    $.fn[ pluginName ] = function ( options ) {
-        return this.each(function() {
-            if ( !$.data( this, "plugin_" + pluginName ) ) {
-                $.data( this, "plugin_" + pluginName, new Plugin( this, options ) );
+
+    $.fn[pluginName] = function (options) {
+        return this.each(function () {
+            if (!$.data(this, "plugin_" + pluginName)) {
+                $.data(this, "plugin_" + pluginName, new Plugin(this, options));
             }
         });
     };
- 
-})( jQuery, window, document );
- 
+
+})(jQuery, window, document);
+
 // ------------------------------
- 
-(function ( $ ) {
+
+(function ($) {
     $(function () {
         $("#banner").banner({
             speed: 1500,
             delay: 1500
         });
     });
-})( jQuery );
+})(jQuery);
